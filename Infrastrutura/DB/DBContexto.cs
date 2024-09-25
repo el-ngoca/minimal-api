4e1ba4f5839cc7e64b126ff0d8b4576a099d67bb
El-ngoca <eduardongoca4@gmail.com>
2024-09-25 22:02:18 +0200

    configuracao e migracao de base de dados concluida

--- Infrastrutura/DB/DBContexto.cs
+++
@@ -1,6 +0,0 @@
-namespace MinimalAPIContexto;
-
-public class DBContexto
-{
-    
-}
---
+++ Infrastrutura/DB/DbContexto.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Entidades.Adm;
+
+namespace MinimalAPIContexto;
+
+public class DbContexto : DbContext
+{
+    private readonly IConfiguration _configuracaoAppSettings;
+
+    public DbContexto (IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+
+    public DbSet<Administrador> Administradores {get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+        if(!string.IsNullOrEmpty(stringConexao))
+        {
+             optionsBuilder.UseMySql(stringConexao, 
+            ServerVersion.AutoDetect(stringConexao));
+
+        }
+       
+    }
+}
